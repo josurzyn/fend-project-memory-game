e11e1410e3776870b7ce3e058441901b083bad63
Jo Surzyn <josurzyn@gmail.com>
2018-04-08 18:11:42 +0100

    feat: Create Cards Array

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const cards = [
+  'fa fa-diamond',
+  'fa fa-paper-plane-o',
+  'fa fa-anchor',
+  'fa fa-bolt',
+  'fa fa-cube',
+  'fa fa-leaf',
+  'fa fa-bicycle',
+  'fa fa-bomb',
+];
 
 /*
  * Display the cards on the page
