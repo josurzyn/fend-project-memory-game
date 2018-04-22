f84824aea2bcb1238eac73371be2c610fec52084
Jo Surzyn <josurzyn@gmail.com>
2018-04-22 15:25:09 +0100

    fix: Correct formatting of timer display

--- js/app.js
+++ js/app.js
@@ -248,6 +248,7 @@ function countTime(){
             seconds = 0;
         }
     }
+    timerSpan.innerHTML = (minutes < 10 ? "0" + minutes : minutes) + ':' + (seconds < 10 ? "0" + seconds : seconds);
     console.log('Seconds: ' + seconds + ' Minutes: ' + minutes);
 }
 
